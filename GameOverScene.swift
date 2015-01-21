69d8f6f8c45e3e4db81164c8ad8381cfd4a014c5
Sam Wong <sam.wong.uk@gmail.com>
2015-01-21 13:22:55 +0000

    first commit

---
+++ GameOverScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.swift
+//  YellowSub
+//
+//  Created by Sam Wong on 19/01/2015.
+//  Copyright (c) 2015 sam wong. All rights reserved.
+//
+
+import UIKit
+
+class GameOverScene: SKScene {
+   
+}
---
+++ YellowSub/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  YellowSub
+//
+//  Created by Sam Wong on 21/01/2015.
+//  Copyright (c) 2015 sam wong. All rights reserved.
+//
+
+import Foundation
